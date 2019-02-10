800c2cae6226cd1fbda6bcdb3948019ac8421fd9
littleapril <34704435+littleapril@users.noreply.github.com>
2019-02-10 12:12:02 +0800

    Update RemoveElement.cpp

--- cpp/RemoveElement.cpp
+++ cpp/RemoveElement.cpp
@@ -11,3 +11,20 @@ public:
         return i;
     }
 };
+
+=================================================================
+    
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i = 0;
+        int j = nums.size();
+        while (i<j){
+            if (nums[i]==val)
+                nums[i] = nums[--j];
+            else
+                i++;
+        }
+        return j;
+    }
+};
