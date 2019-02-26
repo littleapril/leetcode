1b724abced27e97433ad4c4dda43011c6d7b8f70
littleapril <34704435+littleapril@users.noreply.github.com>
2019-02-26 19:48:27 +0800

    Update Search Insert Position.py

--- python/Search Insert Position.py
+++ python/Search Insert Position.py
@@ -10,3 +10,19 @@ class Solution:
             else:
                 return i
                 
+==========================================================
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums)
+        while left<right:
+            mid = left+(right-left)//2
+            if nums[mid]>target:
+                right = mid
+            elif nums[mid]<target:
+                left = mid + 1
+            else:
+                return mid
+        return left
+    
